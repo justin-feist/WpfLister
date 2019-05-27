e5001e6fbd4a3ccf90911baa11f4c20684941350
Justin Feist <justin.feist@essvote.com>
2019-05-27 01:12:11 -0500

    Initial code commit

---
+++ src/WpfLister/Repository/IListRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WpfLister.Models;
+
+namespace WpfLister.Repository
+{
+    public interface IListRepository
+    {
+
+        Task<IEnumerable<ListItemDto>> GetListItems(int numberToGet);
+
+    }
+}
---
+++ src/WpfLister/Repository/TextListRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using WpfLister.Models;
+
+namespace WpfLister.Repository
+{
+    public class TextListRepository : IListRepository
+    {
+        private readonly string _fileToRead;
+
+        public TextListRepository()
+        {
+            _fileToRead = @".\Data\ListItems.list";
+        }
+
+        public async Task<IEnumerable<ListItemDto>> GetListItems(int numberToGet)
+        {
+            if (numberToGet > 0)
+            {
+                return await Task.Run(() => _GetListItems(_fileToRead));
+            }
+
+            return new List<ListItemDto>();
+        }
+
+        private IEnumerable<ListItemDto> _GetListItems(string fileToRead)
+        {
+            var lines = File.ReadLines(fileToRead);
+
+            int counter = 1;
+            var listItems = new List<ListItemDto>();
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line))
+                    continue;
+
+                listItems.Add(new ListItemDto()
+                {
+                    Id = counter,
+                    DisplayValue = line
+                });
+
+                counter++;
+            }
+
+            return listItems;
+        }
+    }
+}
