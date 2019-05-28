188c5fe7150b5c5e9140019de48f59ae136408a2
Justin Feist <justin.feist@essvote.com>
2019-05-27 20:09:41 -0500

    Added a simple unit test

---
+++ src/ListerTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ListerTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ListerTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f7b17495-90d5-44ca-9b11-e68ea3a4bfe6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/ListerTests/TextListRepositoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using WpfLister;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfLister.Repository;
+
+namespace ListerTests
+{
+    [TestClass]
+    public class TextListRepositoryTests
+    {
+        [TestMethod]
+        public void Get_Valid_Number_Of_List_Items()
+        {
+            var repository = new TextListRepository();
+            var listItems = repository.GetListItems(5).Result;
+
+            //The repository should ignore the blank lines, hence 5 items
+            Assert.AreEqual(5, listItems.Count());
+        }
+    }
+}
